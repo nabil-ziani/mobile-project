d34dec996246b4f6e8059ab04764e384fb55cc9e
Olivier Bex <60689309+olivier-b-be@users.noreply.github.com>
2020-11-16 21:12:07 +0100

    Markers will show popup when selected with some info. Detail button takes you to a more detailed screen about that location.

--- mobile-project/components/MarkerList.js
+++ mobile-project/components/MarkerList.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Marker } from 'react-native-maps';
 
-export default MarkerList = ({ data }) => {
+export default MarkerList = ({ data, setShowDetailPopup, setDetailInfo }) => {
 	return (
 		data.map((marker, index) => (
 			<Marker
 				key={index}
 				coordinate={{ latitude: marker.geometry.coordinates[1], longitude: marker.geometry.coordinates[0] }}
-				title={marker.properties.naam}
+				onPress={() => {
+					setShowDetailPopup(true);
+					setDetailInfo({
+						title: marker.properties.naam,
+						address: `${marker.properties.postcode} ${marker.properties.district}, ${marker.properties.straat} ${marker.properties.huisnummer} `,
+						properties: marker.properties
+					})
+				}}
 			/>
 		))
 	)
