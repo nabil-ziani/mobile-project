119dcaadcf3b7ade1ba4b3f9d861019db0ea1338
Olivier Bex <60689309+olivier-b-be@users.noreply.github.com>
2020-11-19 12:22:31 +0100

    Ask permission for user to use location, if granted show user's location.

--- mobile-project/components/MapScreen.js
+++ mobile-project/components/MapScreen.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, Button, ActivityIndicator } from 'react-native';
 import MapView from "react-native-maps";
 import MarkerList from './MarkerList';
 import { Feather } from "@expo/vector-icons";
+import * as Location from "expo-location";
 
-const ANTWERPEN_COORDS = {
+const COORDS_START = {
   latitude: 51.216667,
   longitude: 4.416078,
   delta: 0.1
@@ -13,42 +14,103 @@ const ANTWERPEN_COORDS = {
 export default MapScreen = ({ navigation, data }) => {
   const [showDetailPopup, setShowDetailPopup] = useState(false);
   const [detailInfo, setDetailInfo] = useState({ title: "", address: "", properties: {} });
+  const [coordinates, setCoordinates] = useState(COORDS_START);
+  const [loading, setLoading] = useState(true);
+  const [userlocation, setUserlocation] = useState(false);
+
+  const getGeoLocation = async () => {
+    try {
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status === "granted") {
+        const location = await Location.getCurrentPositionAsync({});
+        setCoordinates({ latitude: location.coords.latitude, longitude: location.coords.longitude, delta: 0.1 });
+        setUserlocation(true);
+      } else {
+        setErrorMsg("Permission to access location was denied");
+      }
+    } catch (ex) {
+      console.error(ex);
+    }
+
+    setLoading(false);
+  }
+
+  const changeCoords = (coord) => {
+    setCoordinates({ latitude: coord.latitude, longitude: coord.longitude, delta: 0.1 })
+  }
+
+  useEffect(() => {
+    getGeoLocation();
+  }, []);
+
+  // Toont een standaard map zonder punten, na locatie ophalen zal standaard antwerpen coordinaten gebruiken of de gebruiker's coordinaten met bijbehorende markers van de api
 
   return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.container}
-        initialRegion={{
-          latitude: ANTWERPEN_COORDS.latitude,
-          longitude: ANTWERPEN_COORDS.longitude,
-          latitudeDelta: ANTWERPEN_COORDS.delta,
-          longitudeDelta: ANTWERPEN_COORDS.delta
-        }}>
-
-        {/* Data undefined voordat de json fetch wordt binnen gehaald */}
-        {data.length > 0 && <MarkerList data={data} setShowDetailPopup={setShowDetailPopup} setDetailInfo={setDetailInfo} />}
-
-      </MapView>
-      {
-        showDetailPopup &&
-        <View style={styles.popup}>
-          <View style={styles.detailbox}>
-            <View style={{ flex: 0.9 }}>
-              <Text style={styles.locationName}>{detailInfo.title}</Text>
-              <Text style={styles.locationAddress}>{detailInfo.address}</Text>
-            </View>
-            <Feather style={{ flex: 0.1 }} name="x-square" size={35} color="red" onPress={() => setShowDetailPopup(false)} />
-          </View>
-          <Button title="Detail" onPress={() => navigation.navigate("Detail", { itemInfo: detailInfo.properties })}></Button>
+    loading
+      ? <View style={styles.container}>
+        <MapView
+          style={styles.container}
+          initialRegion={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            latitudeDelta: coordinates.delta,
+            longitudeDelta: coordinates.delta
+          }}
+        />
+        <View style={styles.loading}>
+          <ActivityIndicator color="000" size="large" />
         </View>
-      }
-    </View>
+      </View>
+      :
+      <View style={styles.container}>
+        <MapView
+          style={styles.container}
+          region={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            latitudeDelta: coordinates.delta,
+            longitudeDelta: coordinates.delta
+          }}
+          showsUserLocation={userlocation}
+          onMarkerPress={(event) => changeCoords(event.nativeEvent.coordinate)}
+        >
+
+          {/* Data undefined voordat de json fetch wordt binnen gehaald */}
+          {data.length > 0 && <MarkerList data={data} setShowDetailPopup={setShowDetailPopup} setDetailInfo={setDetailInfo} />}
+
+        </MapView>
+        {
+          showDetailPopup &&
+          <View style={styles.popup}>
+            <View style={styles.detailbox}>
+              <View style={{ flex: 0.9 }}>
+                <Text style={styles.locationName}>{detailInfo.title}</Text>
+                <Text style={styles.locationAddress}>{detailInfo.address}</Text>
+              </View>
+              <Feather style={{ flex: 0.1 }} name="x-square" size={35} color="red" onPress={() => setShowDetailPopup(false)} />
+            </View>
+            <Button title="Detail" onPress={() => navigation.navigate("Detail", { itemInfo: detailInfo.properties })}></Button>
+          </View>
+        }
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  loading: {
+    backgroundColor: "white",
+    opacity: 0.8,
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems: "center",
+    justifyContent: "center"
   },
   popup: {
     backgroundColor: "white",
