d86b75eea5f10fd691089e743100a37b720f97cd
Nabil <nabil_ee@hotmail.com>
2020-11-20 13:09:26 +0100

    configuration for android

--- mobile-project/components/CameraScreen.js
+++ mobile-project/components/CameraScreen.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState, useRef } from 'react';
 import { Camera } from 'expo-camera';
 import { View, Button, StyleSheet, Text } from "react-native";
 import * as FileSystem from 'expo-file-system';
+import * as Permissions from "expo-permissions";
 
 export default CameraScreen = ({navigation, route}) => {
   const [hasPermission, setHasPermission] = useState();
+  const [hasAccess, setHasAccess] = useState();
   const camera = useRef();
 
   const takePicture = async () => {
     let picture = await camera.current.takePictureAsync();
+    const { status } = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+    setHasAccess(status === 'granted');
+    console.log(status);
     // storing image in app's file system with locationId as name
     try {
       let location = route.params.itemInfo;
@@ -25,16 +30,21 @@ export default CameraScreen = ({navigation, route}) => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === 'granted');
+      return;
     })();
   }, []);
 
-  if (hasPermission === null) {
+  if (hasPermission === null || hasAccess === null) {
     console.log(hasPermission);
     return <View />
   }
   if (hasPermission === false) {
     console.log(hasPermission);
     return <Text>No access to camera</Text>;
+  }
+  if (hasAccess === false) {
+    console.log(hasAccess);
+		return <Text>No access to photo library</Text>;
   }
 
   return (
